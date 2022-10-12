fd717037341fa5a31f6cbebac7869f210621f094
Do Tien <tiennt1242@gmail.com>
2022-10-12 15:04:52 +0700

    lalala

--- src/js/app.js
+++ src/js/app.js
@@ -12,3 +12,26 @@ function dropdown() {
         display.style.display = 'none';
     }
 }
+
+
+// for mobile screen
+document.getElementById('more-options').addEventListener('click', openBoardOptions);
+function openBoardOptions() {
+    var display = document.getElementById("board-options");
+    var style = getComputedStyle(display);
+    if (style.display == 'none') {
+        display.style.display = 'block';
+    } else {
+        display.style.display = 'none';
+    }
+}
+document.getElementById('dropdown-mobile').addEventListener('click', droplistMobile);
+function droplistMobile() {
+    var display = document.getElementById('droplist-mobile');
+    var style = getComputedStyle(display);
+    if (style.display == 'none') {
+        display.style.display = 'block';
+    } else {
+        display.style.display = 'none';
+    }
+}
